536b5a530d69706b7db93d1495e406466d7804ac
sparks333 <76711232+sparks333@users.noreply.github.com>
2023-12-20 10:46:30 -0800

    Whitespace cleanup

--- Day20/day20.rs
+++ Day20/day20.rs
@@ -1,44 +1,30 @@
 use std::fs::read_to_string;
 use std::collections::HashMap;
-use num::integer::lcm;
 
 fn main() {
     let mut network:HashMap<String, (char, HashMap<String, i32>, i32, Vec<String>)> = HashMap::new();
-    for line in read_to_string("input.txt").unwrap().lines()
-    {
+    for line in read_to_string("input.txt").unwrap().lines() {
         let split_line:Vec<String> = line.split([' ', '-', '>', ',']).filter(|&x| !x.is_empty()).map(str::to_string).collect();
         let node_name:String;
         let node_type:char;
         let mut node_dest:Vec<String> = vec![];
-        if split_line[0] == "broadcaster"
-        {
+        if split_line[0] == "broadcaster" {
             node_type = 'b';
-            node_name = split_line[0].clone();
-        }
-        else
-        {
+            node_name = split_line[0].clone(); 
+        } else {
             node_type = split_line[0].chars().collect::<Vec<_>>()[0];
             node_name = String::from_iter(split_line[0].chars().collect::<Vec<_>>()[1..].to_vec().iter());
         }
-        for i in 1..split_line.len()
-        {
-            node_dest.push(split_line[i].clone());
-        }
+        for i in 1..split_line.len() { node_dest.push(split_line[i].clone()); }
         network.insert(node_name, (node_type, HashMap::new(), 0, node_dest));
     }
 
     let net2 = network.clone();
 
-    for n in &mut network
-    {
-        if n.1.0 == '&'
-        {
-            for m in &net2
-            {
-                if m.1.3.contains(&n.0)
-                {
-                    n.1.1.insert(m.0.clone(), -1);
-                }
+    for n in &mut network {
+        if n.1.0 == '&' {
+            for m in &net2 {
+                if m.1.3.contains(&n.0) { n.1.1.insert(m.0.clone(), -1); }
             }
         }
     }
@@ -50,83 +36,39 @@ fn main() {
 
     let mut pulses = vec![];
 
-    for _i in 0..1000
-    {
+    for _i in 0..1000 {
         pulses.push(("broadcaster".to_string(), "".to_string(), -1));
-        loop
-        {
-            if pulses.len() == 0
-            {
-                break;
-            }
+        loop {
+            if pulses.len() == 0 { break; }
             let p = pulses.pop().unwrap();
-            if p.2 == -1
-            {
-                neg_pulse += 1;
-            }
-            else
-            {
-                pos_pulse += 1;
-            }
-            if network.contains_key(&p.0)
-            {
+            if p.2 == -1 { neg_pulse += 1; } else { pos_pulse += 1; }
+            if network.contains_key(&p.0) {
                 let node = network.get_mut(&p.0).unwrap();
-                if node.0 == '%'
-                {
-                    if p.2 == -1
-                    {
-                        if node.2 == 0
-                        {
+                if node.0 == '%' {
+                    if p.2 == -1 {
+                        if node.2 == 0 {
                             node.2 = 1;
-                            for receipient in &node.3
-                            {
-                                pulses.push((receipient.clone(), p.0.clone(), 1));
-                            }
-                        }
-                        else
-                        {
+                            for receipient in &node.3 { pulses.push((receipient.clone(), p.0.clone(), 1)); }
+                        } else {
                             node.2 = 0;
-                            for receipient in &node.3
-                            {
-                                pulses.push((receipient.clone(), p.0.clone(), -1));
-                            }
+                            for receipient in &node.3 { pulses.push((receipient.clone(), p.0.clone(), -1)); }
                         }
                     }
                 }
-                if node.0 == '&'
-                {
+                if node.0 == '&' {
                     let sender = node.1.get_mut(&p.1).unwrap();
                     *sender = p.2;
 
                     let mut all_pos = true;
-                    for val in &node.1
-                    {
-                        if *val.1 == -1
-                        {
-                            all_pos = false;
-                        }
-                    }
-                    if all_pos
-                    {
-                        for receipient in &node.3
-                        {
-                            pulses.push((receipient.clone(), p.0.clone(), -1));
-                        }
-                    }
-                    else
-                    {
-                        for receipient in &node.3
-                        {
-                            pulses.push((receipient.clone(), p.0.clone(), 1));
-                        }
+                    for val in &node.1 { if *val.1 == -1 { all_pos = false; } }
+                    if all_pos {
+                        for receipient in &node.3 { pulses.push((receipient.clone(), p.0.clone(), -1)); }
+                    } else {
+                        for receipient in &node.3 { pulses.push((receipient.clone(), p.0.clone(), 1)); }
                     }
                 }
-                if node.0 == 'b'
-                {
-                    for receipient in &node.3
-                    {
-                        pulses.push((receipient.clone(), p.0.clone(), p.2));
-                    }
+                if node.0 == 'b' {
+                    for receipient in &node.3 { pulses.push((receipient.clone(), p.0.clone(), p.2)); }
                 }
             }
         }
@@ -144,137 +86,64 @@ fn main() {
     let mut lz_button_presses = 0;
     let mut kh_button_presses = 0;
 
-    loop
-    {
+    loop {
         pulses.push(("broadcaster".to_string(), "".to_string(), -1));
         button_presses += 1;
         
-        loop
-        {
-            if pulses.len() == 0
-            {
-                break;
-            }
+        loop {
+            if pulses.len() == 0 { break; }
             let p = pulses.pop().unwrap();
 
-            if p.0 == "cs" && p.1 == "hn" && p.2 == 1
-            {
-                if hn_button_presses > 0
-                {
-                    hn_button_presses = button_presses+1;
-                }
-                else
-                {
-                    hn_button_presses = button_presses;
-                }
-                println!("Part 2: hn {}", hn_button_presses);
+            if p.0 == "cs" && p.1 == "hn" && p.2 == 1 {
+                if hn_button_presses > 0 { hn_button_presses = button_presses+1; } else { hn_button_presses = button_presses; }
             }
 
-            if p.0 == "cs" && p.1 == "lz" && p.2 == 1
-            {
-                if lz_button_presses > 0
-                {
-                    lz_button_presses = button_presses+1;
-                }
-                else
-                {
-                    lz_button_presses = button_presses;
-                }
-                println!("Part 2: lz {}", lz_button_presses);
+            if p.0 == "cs" && p.1 == "lz" && p.2 == 1 {
+                if lz_button_presses > 0 { lz_button_presses = button_presses+1; } else { lz_button_presses = button_presses; }
             }
 
-            if p.0 == "cs" && p.1 == "kh" && p.2 == 1
-            {
-                if kh_button_presses > 0
-                {
-                    kh_button_presses = button_presses+1;
-                }
-                else
-                {
-                    kh_button_presses = button_presses;
-                }                
-                println!("Part 2: kh {}", kh_button_presses);
+            if p.0 == "cs" && p.1 == "kh" && p.2 == 1 {
+                if kh_button_presses > 0 { kh_button_presses = button_presses+1; } else { kh_button_presses = button_presses; }                
             }
 
-            if p.0 == "cs" && p.1 == "tg" && p.2 == 1
-            {
-                if tg_button_presses > 0
-                {
-                    tg_button_presses = button_presses+1;
-                }
-                else
-                {
-                    tg_button_presses = button_presses;
-                }                
-                println!("Part 2: tg {}", tg_button_presses);
+            if p.0 == "cs" && p.1 == "tg" && p.2 == 1 {
+                if tg_button_presses > 0 { tg_button_presses = button_presses+1; } else { tg_button_presses = button_presses; }                
             }
 
-            if hn_button_presses > 0 && lz_button_presses > 0 && kh_button_presses > 0 && tg_button_presses > 0
-            {
+            if hn_button_presses > 0 && lz_button_presses > 0 && kh_button_presses > 0 && tg_button_presses > 0 {
                 let tot:i64 = hn_button_presses * lz_button_presses * kh_button_presses * tg_button_presses;
                 println!("Part 2: {}", tot);
                 return;
             }
 
-            if network.contains_key(&p.0)
-            {
+            if network.contains_key(&p.0) {
                 let node = network.get_mut(&p.0).unwrap();
-                if node.0 == '%'
-                {
-                    if p.2 == -1
-                    {
-                        if node.2 == 0
-                        {
+                if node.0 == '%' {
+                    if p.2 == -1 {
+                        if node.2 == 0 {
                             node.2 = 1;
-                            for receipient in &node.3
-                            {
-                                pulses.push((receipient.clone(), p.0.clone(), 1));
-                            }
-                        }
-                        else
-                        {
+                            for receipient in &node.3 { pulses.push((receipient.clone(), p.0.clone(), 1)); }
+                        } else {
                             node.2 = 0;
-                            for receipient in &node.3
-                            {
-                                pulses.push((receipient.clone(), p.0.clone(), -1));
-                            }
+                            for receipient in &node.3 { pulses.push((receipient.clone(), p.0.clone(), -1)); }
                         }
                     }
                 }
-                if node.0 == '&'
-                {
+                if node.0 == '&' {
                     let sender = node.1.get_mut(&p.1).unwrap();
                     *sender = p.2;
-
                     let mut all_pos = true;
-                    for val in &node.1
-                    {
-                        if *val.1 == -1
-                        {
-                            all_pos = false;
-                        }
+                    for val in &node.1 {
+                        if *val.1 == -1 { all_pos = false; }
                     }
-                    if all_pos
-                    {
-                        for receipient in &node.3
-                        {
-                            pulses.push((receipient.clone(), p.0.clone(), -1));
-                        }
-                    }
-                    else
-                    {
-                        for receipient in &node.3
-                        {
-                            pulses.push((receipient.clone(), p.0.clone(), 1));
-                        }
+                    if all_pos {
+                        for receipient in &node.3 { pulses.push((receipient.clone(), p.0.clone(), -1)); }
+                    } else {
+                        for receipient in &node.3 { pulses.push((receipient.clone(), p.0.clone(), 1)); }
                     }
                 }
-                if node.0 == 'b'
-                {
-                    for receipient in &node.3
-                    {
-                        pulses.push((receipient.clone(), p.0.clone(), p.2));
-                    }
+                if node.0 == 'b' {
+                    for receipient in &node.3 { pulses.push((receipient.clone(), p.0.clone(), p.2)); }
                 }
             }
         }
